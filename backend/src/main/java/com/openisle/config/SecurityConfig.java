c47c318e6f2b13be67540c7ae1e938669efb85e3
tim <nagisa12164@163.com>
2025-09-23 23:31:53 +0800

    fix: 简单更新本地调试端口

--- backend/src/main/java/com/openisle/config/SecurityConfig.java
+++ backend/src/main/java/com/openisle/config/SecurityConfig.java
@@ -101,8 +101,8 @@ public class SecurityConfig {
         "http://localhost",
         "http://30.211.97.238:3000",
         "http://30.211.97.238",
-        "http://192.168.7.98",
-        "http://192.168.7.98:3000",
+        "http://192.168.7.90",
+        "http://192.168.7.90:3000",
         "https://petstore.swagger.io",
         // 允许自建OpenAPI地址
         "https://docs.open-isle.com",
