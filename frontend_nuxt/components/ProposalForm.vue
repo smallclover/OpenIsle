90649b422df6690453745be3ef03866149463c58
Tim <nagisa12164@163.com>
2025-10-22 20:39:54 +0800

    feat: 补充提案规则ui

--- frontend_nuxt/components/ProposalForm.vue
+++ frontend_nuxt/components/ProposalForm.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="proposal-section">
     <div class="proposal-row">
+      <span class="proposal-row-title rule">
+        <info-icon class="proposal-description-title-icon" />提案规则说明</span
+      >
+      <div class="proposal-description-content">
+        <p>提案规则1</p>
+        <p>提案规则</p>
+        <p>提案规则</p>
+        <p>提案规则</p>
+      </div>
+    </div>
+    <div class="proposal-row">
       <span class="proposal-row-title">拟议分类名称</span>
       <BaseInput v-model="data.proposedName" placeholder="请输入分类名称" />
     </div>
@@ -35,8 +46,32 @@ defineProps({
   margin-bottom: 10px;
 }
 
+.proposal-row-title.rule {
+  margin-bottom: 0;
+}
+
 .proposal-row {
   display: flex;
   flex-direction: column;
+}
+
+.proposal-activity {
+  margin-top: 20px;
+  padding: 20px;
+}
+
+.proposal-description-title-text {
+  font-size: 14px;
+  font-weight: bold;
+  margin-left: 5px;
+}
+
+.proposal-description-title-icon {
+  margin-right: 5px;
+}
+
+.proposal-description-content {
+  font-size: 12px;
+  opacity: 0.8;
 }
 </style>
